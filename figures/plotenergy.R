61249b6f731f7c31d9449f827df6a948104ffb8a
Dennis Evangelista <devangel77b@gmail.com>
2026-05-03 16:33:24 -0400

    blah

--- figures/plotenergy.R
+++ figures/plotenergy.R
@@ -7,24 +7,24 @@ data <- tibble(raw)
 
 # happy energy figure
 plotdata <- filter(data,type=='happy')
-fig7a <- ggplot(plotdata, aes(x=etype,y=E,fill=etype))+
+fig7a <- ggplot(plotdata, aes(x=etype,y=E,alpha=etype))+
       geom_hline(yintercept=0,color="gray70")+
-      stat_summary(fun=mean,geom="bar",color="gray70")+
-      stat_summary(fun.data="mean_sdl",fun.args=list(mult=1),geom="errorbar",width=0.2)+
+      stat_summary(fun=mean,geom="bar",color="gray70",fill='#f8766d')+
+      scale_alpha_manual(values=c(1.0,0.5,0.15))+
+      stat_summary(fun.data="mean_sdl",fun.args=list(mult=1),geom="errorbar",width=0.2,alpha=1)+
       ylab('energy, \\unit{\\joule}')+
-      scale_fill_brewer(palette="PuRd",direction=-1)+
       theme_bw(base_size=8)+
       theme(legend.position="none",axis.title.x=element_blank())
 ggsave('fig7a.svg',plot=fig7a,width=3.4167,height=2,units="in")
 
 # sad energy figure
 plotdata <- filter(data,type=='sad')
-fig7b <- ggplot(plotdata, aes(x=etype,y=E,fill=etype))+
+fig7b <- ggplot(plotdata, aes(x=etype,y=E,alpha=etype))+
       geom_hline(yintercept=0,color="gray70")+
-      stat_summary(fun=mean,geom="bar",color="gray70")+
-      stat_summary(fun.data="mean_sdl",fun.args=list(mult=1),geom="errorbar",width=0.2)+
+      stat_summary(fun=mean,geom="bar",color="gray70",fill='#00bfc4')+
+      scale_alpha_manual(values=c(1.0,0.5,0.15))+
+      stat_summary(fun.data="mean_sdl",fun.args=list(mult=1),geom="errorbar",width=0.2,alpha=1)+
       ylab('energy, \\unit{\\joule}')+
-      scale_fill_brewer(palette="GnBu",direction=-1)+
       theme_bw(base_size=8)+
       theme(legend.position="none",axis.title.x=element_blank())
 ggsave('fig7b.svg',plot=fig7b,width=3.4167,height=2,units="in")
